e0f42c9bf5b2f63a36286fabccbf018c1c6ec9be
Eujene Yum <eujeneyum@Eujenes-MacBook-Pro.local>
2022-10-14 21:22:24 -0500

    added comments

--- week_03/homework.js
+++ week_03/homework.js
@@ -12,36 +12,36 @@ d3.csv("hw_data.csv").then(data=> {
 
 
     /* create SVG */
-    let svg = d3.select("#bar_chart") // pairs to index.html dv id = chart
+    /* this connects us to homework.html's div tag */
+    let svg = d3.select("#bar_chart") 
         .append("svg")
         .attr("viewBox",[0, 0, width, height]);
 
-    // x axis: library branches
+    /* setting up x axis: library branches */
     let x = d3.scaleBand()
         .domain(data.map(d => d.branch)) 
-        /* start building where margin starts, not where the svd starts */
         .range([margin.left, width - margin.right]) 
-        /* how far apart you want bar to be */
         .padding(0.2);
 
+    /* setting up y axis: number of visits */
     let y = d3.scaleLinear()
-        // domain ranges from 0 to max value of cases
+        /* domain ranges from 0 to max number of visits */
         .domain([0, d3.max(data, d => d.num)])
         .range([height-margin.bottom, margin.top])
         ;
 
-    /* manipulate the x axis */
+    /* append the x axis */
     svg.append("g")
         .attr("transform", `translate(0,${height - margin.bottom + 5})`) // move location of axis
         .call(d3.axisBottom(x));
 
     
-    /* manipulate the y axis */
+    /* append the y axis */
     svg.append("g")
         .attr("transform", `translate(${margin.left - 5},0)`)
         .call(d3.axisLeft(y));
     
-    /* add text element */
+    /* add text element - Title */
     svg.append("text")
         .attr("transform", "translate(240,0)")
         .attr("x", 50)
@@ -49,7 +49,7 @@ d3.csv("hw_data.csv").then(data=> {
         .attr("font-size", "24px")
         .text("Library Branch Visits")
 
-    /* add y axis */
+    /* add text element - y axis */
     svg.append("text")
         .attr("class", "y label")
         .attr("text-anchor", "end")
@@ -58,23 +58,25 @@ d3.csv("hw_data.csv").then(data=> {
         .attr("transform", "rotate(-90)")
         .text("# of visits");
 
-    /* add x axis */
+     /* add text element - x axis */
     svg.append("text")
         .attr("class", "x label")
         .attr("text-anchor", "end")
         .attr("x", width-30)
         .attr("y", height + margin.top)
         .text("Library Branches");
-        
+    
+    /* create bar element */ 
     let bar = svg.selectAll(".bar")
-        .append("g") // append group 
+        .append("g") 
         .data(data)
         .join("g")
         .attr("class","bar");
 
+    /* add bar element to svg */
     bar.append("rect")
         .attr("fill", "cornflowerblue")
-        .attr("x", d => x(d.branch)) // building suing the x scale we created 
+        .attr("x", d => x(d.branch)) 
         .attr("width", x.bandwidth())
         .attr("y", d => y(d.num))
         .attr("height", d => y(0) - y(d.num));  
