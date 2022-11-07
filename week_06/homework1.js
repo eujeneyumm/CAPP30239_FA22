57ae724327f91b489269c6486e67b7dd8bd65537
Eujene Yum <eujeneyum@Eujenes-MacBook-Pro.local>
2022-11-07 17:25:34 -0600

    adding graph 1

--- week_06/homework1.js
+++ week_06/homework1.js
@@ -4,51 +4,45 @@ const height = 600,
     width = 800,
     margin = ({ top: 25, right: 30, bottom: 35, left: 50 });
 
-d3.json("a3cleanedonly2015.json").then(data => {
-    let timeParse = d3.timeParse("%x");
-    let formatDate = d3.timeFormat("%m")
+d3.csv("chart1_data.csv").then(data => {
+    let timeParse = d3.timeParse("%m");
+    // let formatDate = d3.timeFormat("%m")
 
-    console.log(data)
+    // make month d3 time date and counts numerical
     for (let d of data) {
-        d.Date = timeParse(d.Date); // create new var Month (from Date)
-        d.Month = formatDate(d.Date)
+        d.count = +d.count
+        d.Month = timeParse(d.month); // create new var Month (from Date)
     };
-
-    // Aggregate (count) number of shootings per month
-    var month_cnt = d3.rollup(data, g => g.length, d => d.Month);
-    var agg = Array.from(month_cnt, ([month, count]) => ({ month, count }));
 
     let svg = d3.select("#hw1_chart")
         .append("svg")
         .attr("viewBox", [0, 0, width, height]); // resize element in browser
 
-    // scaleBand for bar chart categories
-    let x = d3.scaleBand()
-        .domain(data.map(d => d.Month).sort()) // need to sort months
-        .range([margin.left, width - margin.right]) // pixels on page
-        .padding(0.1);
+    let x = d3.scaleTime()
+        .domain(d3.extent(data, d => d.Month)) // extent gives min & max, returns as array
+        .range([margin.left, width - margin.right]);
     
     // linear b/c num (y) is numerical 
     let y = d3.scaleLinear()
-        .domain([0, d3.max(agg, d => d.count)]).nice() // nice rounds the top num on y axis
+        .domain([0, d3.max(data, d => d.count)]).nice() // nice rounds the top num on y axis
         .range([height - margin.bottom, margin.top]);
-    
-    /* Update: simplfied axes */
+
     svg.append("g")
-        .attr("transform", `translate(0,${height - margin.bottom + 5})`) // move location of axis
-        .call(d3.axisBottom(x));
-    
+        .attr("transform", `translate(0,${height - margin.bottom})`) // transform translate to put axis in right place
+        .call(d3.axisBottom(x).tickSizeOuter(0).tickFormat(d3.timeFormat("%b"))); // tick size outer removes weird hanging tick @ end of axis; use month abbr
+      
     svg.append("g")
-        .attr("transform", `translate(${margin.left - 5},0)`)
-        .call(d3.axisLeft(y));
-    
+        .attr("transform", `translate(${margin.left},0)`)
+        .attr('class', 'y-axis')
+        .call(d3.axisLeft(y).tickSizeOuter(0).tickSize(-width));
+  
     svg.append("text")
         .attr("class", "x-label")
         .attr("text-anchor", "end")
         .attr("x", width - margin.right)
         .attr("y", height)
         .attr("dx", "0.5em")
-        .attr("dy", "-0.05em") 
+        .attr("dy", "-0.25em") 
         .text("Month");
       
     svg.append("text")
@@ -59,25 +53,17 @@ d3.json("a3cleanedonly2015.json").then(data => {
         .attr("y", 10)
         .attr("transform", "rotate(-90)")
         .text("# Police Shootings");
-
-    let bar = svg.selectAll(".bar") // create bar groups
-        .append("g")
-        .data(agg)
-        .join("g")
-        .attr("class", "bar");
-
-    bar.append("rect") // add rect to bar group
-        .attr("fill", "steelblue")
-        .attr("x", d => x(d.month)) // x position attribute
-        .attr("width", x.bandwidth()) // this width is the width attr on the element
-        .attr("y", d => y(d.count)) // y position attribute
-        .attr("height", d => y(0) - y(d.count)); // this height is the height attr on element
-    
-    bar.append('text') // add labels to bars
-        .text(d => d.count)
-        .attr('x', d => x(d.month) + (x.bandwidth()/2))
-        .attr('y', d => y(d.count) + 15)
-        .attr('text-anchor', 'middle')
-        .style('fill', 'white');
+  
+      // put line on page!
+    let line = d3.line()
+          .x(d => x(d.Month))
+          .y(d => y(d.count))
+          .curve(d3.curveNatural);  // curve the line
+  
+    svg.append('path')
+          .datum(data)
+          .attr("d", line)
+          .attr('fill', 'none')
+          .attr('stroke', 'steelblue')
 
 });
